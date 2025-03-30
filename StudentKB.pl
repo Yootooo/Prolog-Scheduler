ee09d742b768ec7de60ef44c6149eee9d6a8399c
Ziad-Abdou1 <ziad.abdou1980@gmail.com>
2025-03-30 13:50:17 +0200

    student_schedule

--- StudentKB.pl
+++ StudentKB.pl
@@ -19,6 +19,29 @@ day_schedule(tuesday, [[signals402],[physics201, csen202],[],[],[]]).
 day_schedule(wednesday, [[math203],[],[physics201, elct402],[],[]]).
 day_schedule(thursday, [[math203],[],[],[netw201, csen202],[]]).
 
+
+find_slot(Course, [SlotCourses | _], SlotNumber, SlotNumber) :-
+    member(Course, SlotCourses), !. 
+find_slot(Course, [_ | Rest], N, SlotNumber) :-
+    N1 is N + 1,                 
+    find_slot(Course, Rest, N1, SlotNumber).
+	
+student_schedule(Student, Slots) :-
+    setof(slot(Day, SlotNumber, Course), 
+          (studies(Student, Course), 
+           day_schedule(Day, Schedule), 
+           find_slot(Course, Schedule, 1, SlotNumber)), 
+          Slots),
+		  
+    no_clashes(Slots),     
+    study_days(Slots, 5).  
+	
+	
+university_schedule(S) :-
+    setof(Student, Course^studies(Student, Course), Students), 
+    setof(sched(Student, Slots), 
+          (member(Student, Students), student_schedule(Student, Slots)), S).
+		  
 count_days([],_,0).
 count_days([slot(X,_,_)|T],Acc,N):-
 	\+ member(X,Acc),
@@ -30,6 +53,7 @@ count_days([slot(X,_,_)|T],Acc,N):-
 study_days(Slots,DayCount):-
 	count_days(Slots,[],N),
 	N=<DayCount.
+	
 no_clashes([],_).
 no_clashes([slot(X,Y,_)|T],Acc):-
 	\+ member(pair(X,Y),Acc),
