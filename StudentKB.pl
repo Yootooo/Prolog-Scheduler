ab5d1a113a23b79154ced1ae7de79afbc5e92e48
Ziad-Abdou1 <ziad.abdou1980@gmail.com>
2025-03-30 13:59:03 +0200

    Update StudentKB.pl

--- StudentKB.pl
+++ StudentKB.pl
@@ -20,28 +20,6 @@ day_schedule(wednesday, [[math203],[],[physics201, elct402],[],[]]).
 day_schedule(thursday, [[math203],[],[],[netw201, csen202],[]]).
 
 
-find_slot(Course, [SlotCourses | _], SlotNumber, SlotNumber) :-
-    member(Course, SlotCourses), !. 
-find_slot(Course, [_ | Rest], N, SlotNumber) :-
-    N1 is N + 1,                 
-    find_slot(Course, Rest, N1, SlotNumber).
-	
-student_schedule(Student, Slots) :-
-    setof(slot(Day, SlotNumber, Course), 
-          (studies(Student, Course), 
-           day_schedule(Day, Schedule), 
-           find_slot(Course, Schedule, 1, SlotNumber)), 
-          Slots),
-		  
-    no_clashes(Slots),     
-    study_days(Slots, 5).  
-	
-	
-university_schedule(S) :-
-    setof(Student, Course^studies(Student, Course), Students), 
-    setof(sched(Student, Slots), 
-          (member(Student, Students), student_schedule(Student, Slots)), S).
-		  
 count_days([],_,0).
 count_days([slot(X,_,_)|T],Acc,N):-
 	\+ member(X,Acc),
